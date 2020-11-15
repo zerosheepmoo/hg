652bba718faae121d75686fb343732c57fd228c3
zerosheepmoo <youngyang@Youngs-MacBook-Pro.local>
2020-11-15 19:37:23 +0900

    dev mode add

--- webpack.config.js
+++ webpack.config.js
@@ -52,5 +52,6 @@ module.exports = {
         new HtmlWebPackPlugin({
             title: 'Excalibur Webpack Sample'
         })
-    ]
+    ],
+    
 }
--- webpack.production.js
+++ webpack.production.js
@@ -4,8 +4,8 @@ const common = require('./webpack.config.js');
 const TerserPlugin = require("terser-webpack-plugin");
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
-
 module.exports = merge(common, {
+    mode: 'production',
     optimization: {
         minimize: true,
         minimizer: [new TerserPlugin()],
