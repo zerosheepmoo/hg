d765dd8aea0384187fcc6e831b54ff4037dd69d4
zerosheepmoo <youngyang@Youngs-MacBook-Pro.local>
2020-11-15 17:31:53 +0900

    test

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-/**
- * App
- */
-export class App {
-    public static sayHello(): void {
-        alert("Hello!!");
-    }
-}
+import { Engine } from "excalibur";
+// game.ts
+
+// Create an instance of the engine.
+// I'm specifying that the game be 800 pixels wide by 600 pixels tall.
+// If no dimensions are specified the game will be fullscreen.
+const game = new Engine({
+  width: 800,
+  height: 600,
+})
+// todo build awesome game here
+
+// Start the engine to begin the game.
+game.start()
+
 
 // for debug
 
